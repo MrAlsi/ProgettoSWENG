d4f0a7eea77070736bcbd2c5f28884924e99f14d
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-10-14 11:13:29 +0200

    Caricare oggetto nel DB

---
+++ src/com/university/client/UtenteService.java
@@ -0,0 +1,2 @@
+package com.university.client;public interface UtenteService {
+}
---
+++ src/com/university/client/UtenteServiceAsync.java
@@ -0,0 +1,6 @@
+package com.university.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface UtenteServiceAsync {
+}
