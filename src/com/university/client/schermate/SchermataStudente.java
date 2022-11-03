62b36d9d7320b4f94b3a4454093b08f8824b393a
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-11-03 18:06:00 +0100

    sistemato il codice per il merge

--- src/com/university/client/schermate/SchermataStudente.java
+++ src/com/university/client/schermate/SchermataStudente.java
@@ -2,8 +2,7 @@ package com.university.client.schermate;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.*;
 import com.university.client.model.Sostiene;
 import com.university.client.model.Studente;
 import com.university.client.services.StudenteService;
@@ -14,17 +13,62 @@ import java.util.ArrayList;
 public class SchermataStudente {
     Studente studente;
     String[] info;
+    VerticalPanel user__container;
+    VerticalPanel nav__user;
     private StudenteServiceAsync serviceStudente = GWT.create(StudenteService.class);
+
 
     public void accesso(Studente studente) {
         RootPanel.get("container").clear();
+
+        user__container = new VerticalPanel();
+        user__container.getElement().setId("user__container");
+        user__container.addStyleName("user__container");
+
+        nav__user = new VerticalPanel();
+        nav__user.getElement().setId("nav__user");
+        nav__user.addStyleName("nav__user");
+
+
         this.studente = studente;
-        caricaInformazioniPersonali();
-        caricaEsamiSvolti();
+
+        Button btn__profilo = new Button("Profilo");
+        Button btn__voti__studente = new Button("Il mio libretto");
+        Button btn__esami = new Button("Pianifico le mie prove");
+        Button btn__esami__studente = new Button("Esami prenotati");
+        Button btn__corsi__studente = new Button("I miei corsi");
+        Button btn__corsi = new Button("Tutti i corsi");
+
+        btn__profilo.addStyleName("nav__user__btn");
+        btn__voti__studente.addStyleName("nav__user__btn");
+        btn__esami.addStyleName("nav__user__btn");
+        btn__esami__studente.addStyleName("nav__user__btn");
+        btn__corsi__studente.addStyleName("nav__user__btn");
+        btn__corsi.addStyleName("nav__user__btn");
+
+        btn__profilo.addClickHandler(clickEvent -> {
+            try {
+                form__profilo();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        nav__user.add(btn__profilo);
+        nav__user.add(btn__voti__studente);
+        nav__user.add(btn__esami);
+        nav__user.add(btn__esami__studente);
+        nav__user.add(btn__corsi__studente);
+        nav__user.add(btn__corsi);
+
+        RootPanel.get("container").add(nav__user);
+        user__container.add(new HTML("<div class=\"user__title\">my<b>University</b></div>"));
+        RootPanel.get("container").add(user__container);
+
     }
 
 
-    public void caricaInformazioniPersonali(){
+    public void form__profilo(){
         //Qua metti il biding della sezione informazioni personali
         final Label[] lab = new Label[1];
         serviceStudente.getInformazioniPersonali(studente, new AsyncCallback<String[]>() {
@@ -32,32 +76,26 @@ public class SchermataStudente {
             public void onFailure(Throwable caught) {
 
             }
-
             @Override
             public void onSuccess(String[] result) {
                 //In result dovresti avere tutti i dati personali dell'utente in un array
                 lab[0] = new Label(result[0]);
+                user__container.clear();
+                HTML user__info = new HTML("<div class=\"content__studente\"><b>Nome: </b>" + result[1]
+                        + "<br /><b>Cognome: </b>" + result[2]
+                        + "<br /><b>E-mail: </b>" + result[0] + "</div>");
+                user__container.add(new HTML("<div class=\"user__title\">Informazioni personali</div>"));
+                user__container.add(user__info);
+                user__container.add(user__info);
+                user__container.add(user__info);
             }
         });
-
-        RootPanel.get("container").add(lab[0]);
+        RootPanel.get("container").add(user__container);
     }
 
     public void caricaEsamiSvolti(){
         //Qua metti il biding della sezione informazioni personali
 
-        serviceStudente.getVoti(studente.matricola, new AsyncCallback<ArrayList<Sostiene>>() {
-            @Override
-            public void onFailure(Throwable caught) {
-
-            }
-
-            @Override
-            public void onSuccess(ArrayList<Sostiene> result) {
-                //In result dovresti un arraylist con tutti gli esami svolti
-                //Metodo calcolaMedia
-            }
-        });
     }
 
     /**
