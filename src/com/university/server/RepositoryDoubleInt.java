f5b43740ca7b996e13afec7beb82e43fa45ad105
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-11-29 11:21:52 +0100

    aggiustato spazi

--- src/com/university/server/RepositoryDoubleInt.java
+++ src/com/university/server/RepositoryDoubleInt.java
@@ -3,7 +3,6 @@ package com.university.server;
 public interface RepositoryDoubleInt<K> {
     public K[] getAll();
     public K[] getArrayById1(int id);
-
     public K[] getArrayById2(int id);
     public boolean Create(K object);
     public boolean Remove(int id1, int id2);
--- src/com/university/server/RepositoryInt.java
+++ src/com/university/server/RepositoryInt.java
@@ -2,18 +2,9 @@ package com.university.server;
 
 public interface RepositoryInt<K>{
     public K GetById(int id);
-
-    //public K GetByString (String stringa );
-
     public K[] getAll();
-
     public boolean Create(K object);
-
     public boolean Remove(int id);
-
-    //public boolean RemoveByString(String stringa);
-
     public boolean Update(K object);
-    //public boolean UpdateByString(K object, String stringa);
 
 }
